8d32956afe5324e89dac822dd6f603dec321c133
cddichristian <christiandaveibanez@yahoo.com>
2026-04-06 15:44:54 +0800

    Create tailwind.config.js with theme customization for water tracker

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: '#00BFFF', // Water color
+        secondary: '#1E90FF', // Lighter water color
+      },
+      spacing: {
+        '80': '20rem', // Custom spacing
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
